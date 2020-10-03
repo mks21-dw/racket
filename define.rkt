c6c29e6dc9b146ee3b96d369e15e051fa8f14c04
JonAlf <jonalf@gmail.com>
2020-10-03 13:08:14 -0400

    added initial solutions

--- define.rkt
+++ define.rkt
@@ -3,6 +3,7 @@
 (define areaC
   (lambda (d)
     (* (/ d 2) (/ d 2) 3.14)))
+"areaC tests"
 (areaC 20) ;==> 314
 (areaC 5) ;==> 19.625
 
@@ -10,11 +11,13 @@
 (define hypotenuse
   (lambda (a b)
     (sqrt (+ (* a a) (* b b)))))
+"hypotenuse tests"
 (hypotenuse 3 4) ;==> 5
 (hypotenuse 11 60) ;==> 61
 
 (define perimFromArea
   (lambda (area)
     (* 4 (sqrt area))))
+"perimFromArea tests"
 (perimFromArea 25) ;==> 20
 (perimFromArea 10) ;==> 12.649110640673518
